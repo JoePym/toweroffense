6afd52822d7c5af13c66632df4e5bd922e682a4e
Joe Pym <hitonagashi@gmail.com>
2013-08-25 18:10:51 -0400

    Added font for spells few more assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Toweroffense::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
